8608175342727ac42f55b13e82108bb431ec1158
Mathieu Desjarlais <github@desmat.ca>
2024-11-17 13:29:56 -0500

    scroll page-body to top on navigate

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useRef } from 'react';
 import Stack from 'react-bootstrap/Stack';
+import { useLocation } from 'react-router';
 import NavFooter from './NavFooter';
 import NavHeader from './NavHeader';
 
@@ -7,12 +9,22 @@ export default function Layout({
 }: {
   children: React.ReactNode
 }) {
+  const pageBodyRef = useRef();
+  const { pathname } = useLocation();
+
+  // scroll page body to top when navigating
+  useEffect(() => {
+    // @ts-ignore
+    pageBodyRef.current && pageBodyRef.current.scrollTo(0, 0);
+  }, [pathname]);
+    
   return (
     <Stack gap={0} className="Page mx-auto">
       <div className="Page-header px-2">
         <NavHeader />
       </div>
-      <div className="Page-body p-2">
+      {/* @ts-ignore */}
+      <div className="Page-body p-2" ref={pageBodyRef}>
         {children}
       </div>
       <div className="Page-footer p-2">
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,11 +2,36 @@ import Logo from '../components/Logo';
 
 export default function About() {
   return (
-    <div className="About">
-        <Logo fill="#282c34" height="15rem"/>
-        <p className="p-4">
-          TODO About page
-        </p>
+    <div className="About p-2">
+      <p className="p-4">
+        <Logo fill="#282c34" height="8rem" />
+      </p>
+      <p className="p-2">
+        This is my submission to LegionDevelopmentGroup's Typescript Assessment.
+      </p>
+      <p className="p-2">
+        This is based on create-react-app and per instructions uses react-boostrap as its UI framework.
+        I used react-query to manage external data access and to a minor extent state management.
+      </p>
+      <p className="p-2">
+        Assessment instructions: XXX
+        <br />
+        Submission repo: XXX
+        <br />
+        Deployed: XXX
+      </p>
+      <p className="p-2">
+        Submitted by: Mathieu Desjarlais
+        <br />
+        Github: XXX
+        <br />
+        Website: XXX
+        <br />
+        Email: XXX
+      </p>
+      <p className="p-4">
+        <Logo fill="#282c34" height="8rem" />
+      </p>
     </div>
   );
 }
