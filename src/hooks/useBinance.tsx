0ad30bee9fbd880516bbc78da6fae43dc8cccf30
Mathieu Desjarlais <github@desmat.ca>
2024-11-17 11:57:55 -0500

    Refactor binance hook; rem splice footgun

--- src/hooks/useBinance.tsx
+++ src/hooks/useBinance.tsx
@@ -2,6 +2,12 @@ import {
   useQuery,
   useQueryClient,
 } from '@tanstack/react-query';
+import {
+  BINANCE_BASE_URL,
+  BINANCE_DEFAULT_INTERVAL,
+  BINANCE_DEFAULT_LIMIT,
+  BINANCE_DEFAULT_SYMBOL
+} from '../types/Binance';
 
 export default function useBinance({
   symbol,
@@ -12,21 +18,20 @@ export default function useBinance({
   interval?: string,
   limit?: number
 } = {
-    symbol: "BTCUSDT",
-    interval: "1d",
-    limit: 180,
+    symbol: BINANCE_DEFAULT_SYMBOL,
+    interval: BINANCE_DEFAULT_INTERVAL,
+    limit: BINANCE_DEFAULT_LIMIT,
   }): any {
   const queryClient = useQueryClient();
+  const queryKey = () => ["klines", symbol, interval, limit];
 
   const query = useQuery({
-    queryKey: ["klines", symbol, interval, limit],
-    staleTime: 5000, // TODO: find appropriate value
+    queryKey: queryKey(),
+    staleTime: 5 * 60 * 1000, // 5 mins
     queryFn: async () => {
-      const res = await fetch(`https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${interval}&limit=${limit}`, {
-        method: "GET",
-      });
-
+      const res = await fetch(`${BINANCE_BASE_URL}?symbol=${symbol}&interval=${interval}&limit=${limit}`);
       console.log("hooks.useBinance useQuery.queryFn", { res });
+
       if (!res.ok) {
         console.error("Query error", { res });
         throw Error(`${res.statusText} (${res.status})`);
@@ -40,15 +45,15 @@ export default function useBinance({
         // for analyzing response
         // openTime180Days: data
         //   .sort((a: any[], b: any[]) => a[0] - b[0])
-        //   .splice(data.length - 180, data.length)
+        //   .slice(data.length - 180, data.length)
         //   .map((entry: any[]) => new Date(entry[0])),
         prices30days: data
           .sort((a: any[], b: any[]) => a[0] - b[0])
-          .splice(data.length - 30, data.length)
+          .slice(data.length - 30, data.length)
           .map((entry: any[]) => [entry[0], parseFloat(entry[4])]),
         volume180days: data
           .sort((a: any[], b: any[]) => a[0] - b[0])
-          .splice(data.length - 180, data.length)
+          .slice(data.length - 180, data.length)
           .map((entry: any[]) => [parseFloat(entry[5]), (entry[2] - entry[3]) / entry[2] * 100]),
       }
     }
@@ -56,7 +61,7 @@ export default function useBinance({
 
   const reload = () => {
     // console.log("hooks.useBinance reload", { });
-    queryClient.resetQueries({ queryKey: ["klines", symbol, interval, limit] });
+    queryClient.resetQueries({ queryKey: queryKey() });
   }
 
   return { query, reload };
---
+++ src/types/Binance.ts
@@ -0,0 +1,4 @@
+export const BINANCE_BASE_URL = "https://api.binance.com/api/v3/klines";
+export const BINANCE_DEFAULT_SYMBOL = "BTCUSDT";
+export const BINANCE_DEFAULT_INTERVAL = "1d";
+export const BINANCE_DEFAULT_LIMIT = 180;
